7c5dcd80bdf85eb313c6a3fdcfac07359fbdc9e9
Daniel Imfeld <daniel@imfeld.dev>
2026-09-21 23:43:25 +0000

    Add mail factory app icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,8 +22,8 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/icons/email-check.svg" type="image/svg+xml" />
-	<link rel="apple-touch-icon" href="/icons/email-check-180.png" />
+	<link rel="icon" href="/icons/mail-factory.svg" type="image/svg+xml" />
+	<link rel="apple-touch-icon" href="/icons/mail-factory-180.png" />
 	<link rel="manifest" href="/manifest.webmanifest" />
 	<meta name="theme-color" content="#07131c" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
